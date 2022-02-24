6f7a3d125da6a4eb6f347d48287583e983a3dc30
Zemmy2 <13zemmy@gmail.com>
2022-02-24 18:30:46 +0700

    Set auth user data on load

--- src/App.js
+++ src/App.js
@@ -5,8 +5,23 @@ import Home from './components/Home/Home';
 import Catalog from './components/Catalog/Catalog';
 import Cart from './components/Cart/Cart';
 import Login from './components/Login/Login';
+import { useEffect } from 'react';
+import { getAuth } from 'firebase/auth';
+import { setAuthUserData } from './redux/auth-reducer';
+import { useDispatch } from 'react-redux';
 
 const App = () => {
+  const dispatch = useDispatch();
+  const auth = getAuth();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        dispatch(setAuthUserData(auth.currentUser));
+      }
+    });
+  }, []);
+
   return (
     <div className='app-wrapper'>
       <Header />
